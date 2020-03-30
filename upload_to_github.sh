de06154dfd1797dc5190f1ed6e9ce48b96c0806c
Hemanta duwarah <hemantaduwarah1@gmail..com>
2020-03-30 19:33:56 +0530

    update

--- upload_to_github.sh
+++ upload_to_github.sh
@@ -1,7 +1,7 @@
 date +"%T" >> /home/hemanta/perl/Extract_corona_data/upload.log
-#perl ../save_webpage.pl
+perl ../save_webpage.pl
 git add .
 git commit -m "update"
-#git push -u origin master
+git push -u origin master
 echo "Upload Done" >> /home/hemanta/perl/Extract_corona_data/upload.log
 
